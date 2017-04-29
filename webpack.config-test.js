a57df0a1c351b59ea10f0dbc66fb501cd38dfdf2
Sławomir Rosiek <rosieks@hotmail.com>
2017-04-29 11:06:56 +0200

    Added initial support for code coverage

--- webpack.config-test.js
+++ webpack.config-test.js
@@ -1,6 +1,8 @@
 var path = require('path');
 var nodeExternals = require('webpack-node-externals');
 
+const coverage = process.env.NODE_ENV === 'coverage';
+
 module.exports = {
     resolve: {
         modules: [path.resolve('./src'), 'node_modules'],
@@ -13,13 +15,18 @@ module.exports = {
     target: 'node',
     externals: [nodeExternals()],
     module: {
-        rules: [
+        rules: [].concat
+            (coverage ? {
+                test: /\.(j|t)sx?$/,
+                include: path.resolve('src'),
+                loader: 'istanbul-instrumenter-loader'
+            } : [],
             { 
-                test: /.tsx?$/, 
+                test: /\.tsx?$/, 
                 exclude: /(node_modules)/,
                 loader: 'ts-loader'
             }
-        ]
+        )        
     },
     devtool: '#inline-cheap-module-source-map'
 };
