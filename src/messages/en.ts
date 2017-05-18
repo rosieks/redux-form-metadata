8615296e623ae8c2d3acf9b908526930240c9c3b
Sławomir Rosiek <rosieks@hotmail.com>
2017-05-18 21:46:11 +0200

    Added missing error messages in english

--- src/messages/en.ts
+++ src/messages/en.ts
@@ -1,16 +1,17 @@
 import { message } from './utils';
 
 export default {
-    required: message((field, param) => `The ${field} field is required`),
-    min: message((field, param) => `The field ${field} must be greather than or equal to ${param}`),
+    required: message((field, param) => `The ${field} field is required.`),
+    min: message((field, param) => `The field ${field} must be greather than or equal to ${param}.`),
     max: message((field, param) => `The field ${field} must be less than or equal to ${param}`),
-    minLength: message((field, param) => `Thie field ${field} must be a string with a minimum length of ${param}`),
-    maxLength: message((field, param) => `Thie field ${field} must be a string with a maximum length of ${param}`),
-    pattern: null,
-    number: null,
-    email: message((field, param) => `The ${field} field is not valid e-mail address`),
-    url: message((field, param) => `The ${field} field is not valid fully-qualified http, https, or ftp URL`),
-    date: message((field, param) => ``),
-    dateISO: message((field, param) => ``),
-    digits: message((field, param) => ``),
+    minLength: message((field, param) => `Thie field ${field} must be a string with a minimum length of ${param}.`),
+    maxLength: message((field, param) => `Thie field ${field} must be a string with a maximum length of ${param}.`),
+    pattern: message((field, param) => `The field ${field} must match the regular expression '${param}'.`),
+    number: message((field, param) => `The ${field} field is not valid number.`),
+    email: message((field, param) => `The ${field} field is not valid e-mail address.`),
+    url: message((field, param) => `The ${field} field is not valid fully-qualified http, https, or ftp URL.`),
+    date: message((field, param) => `The ${field} field is not valid date.`),
+    dateISO: message((field, param) => `The ${field} field is not valid date (ISO).`),
+    digits: message((field, param) => `The ${field} field is not valid natural number.`),
+    equalsTo: message((field, param) => `${field} and ${param} do not match.`),
 };
--- todo.md
+++ todo.md
@@ -1,7 +1,6 @@
 - [x] Accessing other values
 - [x] New rule - equalTo
-- [ ] Class level validation
-- [ ] Error messages in English
+- [x] Error messages in English
 - [ ] Error messages in other languages
 - [x] Error message factories: ```(field, param) => `The field ${field.label} must be a string with a maximum length of ${param}.```
 - [x] Error messages setup on field/rule level
@@ -9,6 +8,7 @@
 - [x] Deep validation
 - [ ] Ability to run validation to
 - [ ] Refactor `describeType` to support extensibility. Validation should be one of extensions.
+- [ ] Class level validation
 
 - [ ] Integration test for JSX/TSX
 - [x] Think about naming - `redux-form-describe` vs `redux-form-metadata` vs `redux-form-annotate`
